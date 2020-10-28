830b334b704b7fc81181393e9e2e20367dac1c1d
lobotskiy <lobotskiy09@gmail.com>
2020-10-28 23:39:34 +0200

    correction 4 task

--- home-work-10/main.js
+++ home-work-10/main.js
@@ -45,30 +45,30 @@
 
 // 4. Вихідний код:
 
-// let server = {
-//     data: 0,
-//     convertToString: function (callback) {
-//         callback((function () {
-//             return this.data + "";
-//         }).bind(this));
-//     }
-// };
-// let client = {
-//     server: server,
-//     result: "",
-//     calc: function (data) {
-//         this.server.data = data;
-//         this.server.convertToString(this.notification());
-//     },
-//     notification: function () {
-//         return (callback => {
-//             this.result = callback();
-//         });
-//     }
-// };
-// client.calc(123);
-// console.log(client.result); // "123"
-// console.log(typeof client.result); // "string"
+let server = {
+    data: 0,
+    convertToString: function (callback) {
+        callback(() => {
+            return this.data + "";
+        });
+    }
+};
+let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+        this.server.data = data;
+        this.server.convertToString(this.notification());
+    },
+    notification: function () {
+        return (callback => {
+            this.result = callback();
+        });
+    }
+};
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
 
 // Змініть код використовуючи стрілочні функції, 
 // щоб в коді не використовувалися методи bind().
